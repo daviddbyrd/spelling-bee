b49ce798d51cc420792d73035bf8795ac5a7bb94
Daniel <dbyrd@tcd.ie>
2025-09-28 19:40:32 +0100

    add random vowel function

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,32 @@
 import WordTile from "./components/WordTile";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [letter, setLetter] = useState('');
+
+  useEffect(() => {
+    randomVowel({excludedVowels: []});
+  }, [])
+
+  const randomVowel = ({excludedVowels}) => {
+    const vowelString = "aeiou";
+    while(true){
+      console.log("hello")
+      const curletter = vowelString[getRandomInt(5)];
+      if(!(excludedVowels.includes(curletter))){
+        setLetter(curletter);
+        return;
+      }
+    }
+  }
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  }
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <WordTile letter="a" />
+      <WordTile letter={letter} />
     </div>
   );
 };
